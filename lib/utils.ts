4981b88c81e552fb8470bc775c04e53f887f186f
SOEPARNO ENTERPRISE <soeparno.corp@outlook.com>
2026-01-29 22:23:17 +0700

    utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,7 +4,7 @@ import { twMerge } from 'tailwind-merge';
 export const protocol =
   process.env.NODE_ENV === 'production' ? 'https' : 'http';
 export const rootDomain =
-  process.env.NEXT_PUBLIC_ROOT_DOMAIN || 'localhost:3000';
+  process.env.NEXT_PUBLIC_ROOT_DOMAIN || 'readtalk.pages.dev';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
